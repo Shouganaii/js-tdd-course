d98072f2b90996d386fa7d1954eec2965e301713
Shouganaii <frias.caio@yahoo.com.br>
2021-04-18 11:32:43 -0300

    criaç

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,38 +1,68 @@
 /* eslint-disable */
-let expect = require('chai').expect;
+import { expect } from 'chai'
+import { div, sub, mult, sum } from '../src/main'
 
-describe('Main', () => {
-  let arr;
+describe('Calc', () => {
+  //smoke tests
 
-  beforeEach(() => {
-    arrr = [1, 2, 3];
-  })
-  it('should have a size of 4 when push another value to the array', () => {
+  describe('Smoke tests', () => {
+    it('should exist the method `sum`', () => {
+      expect(sum).to.exist
+      expect(sum).to.be.a('function')
+    });
 
-    arrr.push(4)
-    expect(arrr).to.have.lengthOf(4)
-  })
+    it('should exist the method `sub`', () => {
+      expect(sub).to.exist
+      expect(sub).to.be.a('function')
+    });
 
-  it('should remove the value 3 when use pop in the array', () => {
+    it('should exist the method `mult`', () => {
+      expect(mult).to.exist
+      expect(mult).to.be.a('function')
+    });
 
-    arrr.pop();
-    expect(arrr).to.not.include(3);
-  })
-
-  it('should remove the value 3 when use pop in the array', () => {
-
-    arrr.pop();
-    expect(arrr).to.have.lengthOf(2)
-  })
-
-  it('should be an array', () => {
-
-    expect(arrr).to.be.an('array')
+    it('should exist the method `div`', () => {
+      expect(div).to.exist
+      expect(div).to.be.a('function')
+    });
 
   })
 
-  it('should return true when pop 3 from the array', () => {
-    expect(arrr.pop() === 3).to.be.true
+  describe('Sum', () => {
+
+    it('should return 4 when sum(2,2)', () => {
+      expect(sum(2, 2)).to.be.equal(4)
+    })
+
   })
+  describe('Sub', () => {
+
+    it('should return 4 when sub(6,2)', () => {
+      expect(sub(6, 2)).to.be.equal(4)
+    })
+
+    it('should return -4 when sub(6,10)', () => {
+      expect(sub(6, 10)).to.be.equal(-4)
+    })
+
+  })
+
+  describe('Mult', () => {
+
+    it('should return 4 when mult(2,2)', () => {
+      expect(mult(2, 2)).to.be.equal(4)
+    })
+
+  })
+
+  describe('Div', () => {
+    it('should return 2 when div(4,2)', () => {
+      expect(div(4, 2)).to.equal(2)
+    })
+    it('it should return `não é possível divisão por zero` when divide by 0 ', () => {
+      expect(div(4, 0)).to.equal('não é possível divisão por zero')
+    })
+  })
+
 
 });
